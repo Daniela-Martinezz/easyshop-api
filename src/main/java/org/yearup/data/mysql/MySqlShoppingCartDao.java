360d95c09553789e91165e725fad0a30c716779c
Daniela Martinez <daniela09986@gmail.com>
2024-12-19 18:37:45 -0700

    update

--- src/main/java/org/yearup/data/mysql/MySqlShoppingCartDao.java
+++ src/main/java/org/yearup/data/mysql/MySqlShoppingCartDao.java
@@ -14,6 +14,7 @@ import java.sql.*;
 import java.util.HashMap;
 import java.util.Map;
 
+//annotations v important
 @Component
 public class MySqlShoppingCartDao extends MySqlDaoBase implements ShoppingCartDao {
 
